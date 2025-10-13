e3d02d3f052e5bcfb8c4decdb5b33a1276c05672
Megan Ennis <mennis0145@gmail.com>
2025-10-13 19:59:19 +0200

    Fix path to wkhtmltopdf binary on production

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,41 +1,27 @@
-# WickedPDF Global Configuration
-#
-# Use this to set up shared configuration options for your entire application.
-# Any of the configuration options shown here can also be applied to single
-# models by passing arguments to the `render :pdf` call.
-#
-# To learn more, check out the README:
-#
-# https://github.com/mileszs/wicked_pdf/blob/master/README.md
-
 WickedPdf.configure do |config|
-  # Use the binary from the gem for cross-platform compatibility
-  begin
-    require 'wkhtmltopdf-binary'
-    gem_spec = Gem.loaded_specs['wkhtmltopdf-binary']
-    if gem_spec
-      gem_path = gem_spec.full_gem_path
-      config.exe_path = File.join(gem_path, 'bin', 'wkhtmltopdf')
-    else
-      # Fallback to trying the gem bin path method
-      config.exe_path = `which wkhtmltopdf`.strip
-      config.exe_path = '/usr/bin/wkhtmltopdf' if config.exe_path.empty?
-    end
-  rescue LoadError
-    # If gem is not available, try system binary
-    config.exe_path = `which wkhtmltopdf`.strip
-    config.exe_path = '/usr/bin/wkhtmltopdf' if config.exe_path.empty?
+  # Detect which wkhtmltopdf gem is available
+  if Gem.loaded_specs['wkhtmltopdf-heroku']
+    # Heroku-specific binary path
+    gem_path = Gem.loaded_specs['wkhtmltopdf-heroku'].full_gem_path
+    config.exe_path = File.join(gem_path, 'bin', 'wkhtmltopdf-heroku')
+  elsif Gem.loaded_specs['wkhtmltopdf-binary']
+    # Local development / test
+    gem_path = Gem.loaded_specs['wkhtmltopdf-binary'].full_gem_path
+    config.exe_path = File.join(gem_path, 'bin', 'wkhtmltopdf')
+  else
+    # Fallback to system binary (if any)
+    config.exe_path = `which wkhtmltopdf`.strip.presence || '/usr/bin/wkhtmltopdf'
   end
-  
-  # Enable local file access for assets
+
+  # Enable local file access
   config.enable_local_file_access = true
-  
-  # Layout file to be used for all PDFs
+
+  # Default layout for PDFs
   config.layout = 'pdf.html'
-  
-  # Set asset host based on environment
+
+  # Asset host configuration
   if Rails.env.production?
-    config.asset_host = "https://#{ENV['HEROKU_APP_NAME']}.herokuapp.com" if ENV['HEROKU_APP_NAME']
+    config.asset_host = "https://#{ENV['HEROKU_APP_NAME']}.herokuapp.com" if ENV['HEROKU_APP_NAME'].present?
   else
     config.asset_host = "http://localhost:3001"
   end
