bc08fb622af298e3989440ecec13ce667030958e
Megan Ennis <mennis0145@gmail.com>
2025-10-13 18:36:22 +0200

    Fix PDF generation on Heroku - remove wkhtmltopdf-heroku gem and add system dependencies

--- Gemfile
+++ Gemfile
@@ -42,8 +42,7 @@ gem 'nio4r', '~> 2.5.9'
 
 # PDF generation
 gem 'wicked_pdf'
-gem 'wkhtmltopdf-binary', '~> 0.12.6', group: [:development, :test]
-gem 'wkhtmltopdf-heroku', '~> 2.12.6', group: [:production]
+gem 'wkhtmltopdf-binary', '~> 0.12.6'
 
 # Optional OCR for receipts
 gem 'rtesseract'
--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -9,27 +9,20 @@
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
 WickedPdf.configure do |config|
-  # Path to the wkhtmltopdf executable: This usually isn't needed if using
-  # one of the wkhtmltopdf-binary family of gems.
-  # config.exe_path = '/usr/local/bin/wkhtmltopdf'
-  #   or
-  # config.exe_path = Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-
-  # Needed for wkhtmltopdf 0.12.6+ to use many wicked_pdf asset helpers
-  # config.enable_local_file_access = true
-
+  # Use the binary from the gem for cross-platform compatibility
+  gem_path = Gem.loaded_specs['wkhtmltopdf-binary'].full_gem_path
+  config.exe_path = File.join(gem_path, 'bin', 'wkhtmltopdf')
+  
+  # Enable local file access for assets
+  config.enable_local_file_access = true
+  
   # Layout file to be used for all PDFs
-  # (but can be overridden in `render :pdf` calls)
-  # config.layout = 'pdf.html'
-
-  # Using wkhtmltopdf without an X server can be achieved by enabling the
-  # 'use_xvfb' flag. This will wrap all wkhtmltopdf commands around the
-  # 'xvfb-run' command, in order to simulate an X server.
-  #
-  # config.use_xvfb = true
-  config.asset_host = "http://localhost:3001" 
-  WickedPdf.config = {
-    layout: 'pdf.html',
-    exe_path: '/usr/local/bin/wkhtmltopdf' # adjust if not found in PATH
-  }
+  config.layout = 'pdf.html'
+  
+  # Set asset host based on environment
+  if Rails.env.production?
+    config.asset_host = "https://#{ENV['HEROKU_APP_NAME']}.herokuapp.com" if ENV['HEROKU_APP_NAME']
+  else
+    config.asset_host = "http://localhost:3001"
+  end
 end
