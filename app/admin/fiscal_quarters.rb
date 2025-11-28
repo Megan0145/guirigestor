1d112cff236a9d47c46dd577df974fc23716e015
Megan Ennis <mennis0145@gmail.com>
2025-11-28 15:40:50 +0100

    Add total outgoing receipt amount to fiscal quarters

--- app/admin/fiscal_quarters.rb
+++ app/admin/fiscal_quarters.rb
@@ -78,6 +78,9 @@ ActiveAdmin.register FiscalQuarter do
           fiscal_quarter.status.humanize
         end
       end
+      row :total_outgoing_receipts do |fiscal_quarter|
+        number_to_currency(fiscal_quarter&.total_amount_outgoing_receipts, unit: available_currencies['EUR'])
+      end
       row :total_tax_submitted do |fiscal_quarter|
         number_to_currency(fiscal_quarter.total_tax_submitted, unit: available_currencies['EUR'])
       end
